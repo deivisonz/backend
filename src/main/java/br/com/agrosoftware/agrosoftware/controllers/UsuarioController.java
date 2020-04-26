660f8e3f59e7eee0f64b8a26dce7552ed364a530
viniciusbarth <viniciusbarthhh@hotmail.com>
2020-04-26 16:13:36 -0300

    Adicionado mapeamento para trazer todos os usuários cadastrados

--- src/main/java/br/com/agrosoftware/agrosoftware/controllers/UsuarioController.java
+++ src/main/java/br/com/agrosoftware/agrosoftware/controllers/UsuarioController.java
@@ -1,10 +1,12 @@
 package br.com.agrosoftware.agrosoftware.controllers;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.agrosoftware.agrosoftware.enums.Funcao;
 import br.com.agrosoftware.agrosoftware.models.Usuario;
+import br.com.agrosoftware.agrosoftware.repositories.UsuarioRepository;
 import br.com.agrosoftware.agrosoftware.services.UsuarioService;
 
 @RestController
@@ -25,9 +28,17 @@ public class UsuarioController {
     
     @Autowired UsuarioService usuarioService;
     
+    @Autowired UsuarioRepository usuarioRepository;
+    
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> find(@PathVariable int id) {
         return ResponseEntity.ok().body(usuarioService.find(id));
+    }
+    
+    @GetMapping("/all")
+    public ResponseEntity<List<Usuario>> findAll() {
+    	List<Usuario> usuariosList = usuarioRepository.findAll();
+        return new  ResponseEntity<List<Usuario>>(usuariosList,HttpStatus.OK);
     }
     
     @PostMapping
