20a06006790694dbed066f3e97b56f8f458c6fea
viniciusbarth <viniciusbarthhh@hotmail.com>
2020-05-06 22:13:10 -0300

    Adicionado mapeamento para trazer todas as propriedades cadastrados

--- src/main/java/br/com/agrosoftware/agrosoftware/controllers/PropriedadeController.java
+++ src/main/java/br/com/agrosoftware/agrosoftware/controllers/PropriedadeController.java
@@ -1,8 +1,12 @@
 package br.com.agrosoftware.agrosoftware.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.agrosoftware.agrosoftware.models.Propriedade;
+import br.com.agrosoftware.agrosoftware.repositories.PropriedadeRepository;
 import br.com.agrosoftware.agrosoftware.services.PropriedadeService;
 
 @RestController
 @RequestMapping("/propriedades")
 public class PropriedadeController {
 	@Autowired PropriedadeService propriedadeService;
-
+	@Autowired PropriedadeRepository propriedadeRepository;
+	
 	@PutMapping("/{id}")
     public ResponseEntity<Void> update(@PathVariable int id, @RequestBody Propriedade propriedade) {
 	    var novaPropriedade = propriedadeService.find(id);
@@ -24,5 +30,11 @@ public class PropriedadeController {
 	    propriedadeService.update(novaPropriedade);
 	    
         return ResponseEntity.noContent().build();
+    }
+	
+    @GetMapping("/allpropriedades")
+    public ResponseEntity<List<Propriedade>> findAll() {
+    	List<Propriedade> propriedadeList = propriedadeRepository.findAll();
+        return new  ResponseEntity<List<Propriedade>>(propriedadeList,HttpStatus.OK);
     }
 }
