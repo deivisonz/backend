70bd740e7466299aa61c2763463a3ae15eba8f7b
deivisonz <deivisonerlacher@hotmail.com>
2020-05-28 22:40:05 -0300

    Update PropriedadeController.java

--- src/main/java/br/com/agrosoftware/agrosoftware/controllers/PropriedadeController.java
+++ src/main/java/br/com/agrosoftware/agrosoftware/controllers/PropriedadeController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.agrosoftware.agrosoftware.models.Propriedade;
-import br.com.agrosoftware.agrosoftware.models.Usuario;
 import br.com.agrosoftware.agrosoftware.repositories.PropriedadeRepository;
 import br.com.agrosoftware.agrosoftware.services.PropriedadeService;
 
